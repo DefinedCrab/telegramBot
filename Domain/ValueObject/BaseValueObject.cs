f65c9c0a324fd63414c84313a1e651b2084d54cb
DefinedCrab <69644361+DefinedCrab@users.noreply.github.com>
2024-04-01 23:27:25 +0300

    Equals reimplementation using deep comparison

--- Domain/ValueObject/BaseValueObject.cs
+++ Domain/ValueObject/BaseValueObject.cs
@@ -1,41 +1,38 @@
 namespace Domain.ValueObject;
 
 public abstract class BaseValueObject {
-     public static bool DeepEquals(object obj, object another) {
-         if (ReferenceEquals(obj, another)) {
-             return true;
-         }
-    
-         if (obj == null || another == null) {
-             return false;
-         }
-    
-         if (obj.GetType() != another.GetType()) {
-             return false;
-         }
-    
-         if (obj.GetType().IsPrimitive && another.GetType().IsPrimitive) {
-             return obj.Equals(another);
-         }
-    
-         var result = true;
-         foreach (var property in obj.GetType().GetProperties()) {
-             var objValue = property.GetValue(obj);
-             var anotherValue = property.GetValue(another);
-             if (objValue == null
-                 || anotherValue == null
-                 || DeepEquals(objValue, anotherValue)) {
-                 result = false;
-             }
-         }
-    
-         return result;
-     }
-    
-    // TODO:Разобраться с сравнением BaseValueObject V
+     
+    // Разобраться с сравнением BaseValueObject V
     public override bool Equals(object? obj) {
-        //TODO:Реализовать DeepCompare (DeepClone)
-        return base.Equals(obj);
+        //done? Реализовать DeepCompare (DeepClone)
+        if (ReferenceEquals(this, obj)) {
+            return true;
+        }
+    
+        if (obj == null) {
+            return false;
+        }
+    
+        if (GetType() != obj.GetType()) {
+            return false;
+        }
+    
+        if (obj.GetType().IsPrimitive && GetType().IsPrimitive) {
+            return Equals(obj);
+        }
+    
+        var result = true;
+        foreach (var property in obj.GetType().GetProperties()) {
+            var objValue = property.GetValue(this);
+            var anotherValue = property.GetValue(obj);
+            if (objValue == null
+                || anotherValue == null
+                || Equals(objValue, anotherValue)) {
+                result = false;
+            }
+        }
+    
+        return result;
     }
 
 
